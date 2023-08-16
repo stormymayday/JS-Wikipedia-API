95cb9f78ef4f14205e91dcf17fb09b861103c88f
stormymayday <stormymayday7@gmail.com>
2023-08-15 20:56:32 -0700

    Rendering cardsList in the DOM

--- js/renderResults.js
+++ js/renderResults.js
@@ -1,10 +1,29 @@
+import getElement from "./utils/getElement.js";
+
+const resultsDiv = getElement('.results');
+
 const renderResults = (list) => {
 
     const cardsList = list.map((item) => {
 
-        console.log(item);
+        const { title, snippet, pageid } = item;
 
-    });
+        return `<a href=http://en.wikipedia.org/?curid=${pageid} target="_blank">
+
+                    <h4>${title}</h4>
+                    <p>${snippet}</p>
+
+                </a>`;
+
+    }).join('');
+
+    resultsDiv.innerHTML = `<!-- '.articles' - start -->
+            <div class="articles">
+
+                ${cardsList}
+
+            </div>
+            <!-- '.articles' - end -->`;
 
 };
 
