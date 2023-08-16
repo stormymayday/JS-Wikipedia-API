991dfde583fe2d2f751bdb27debda64456057ec3
stormymayday <stormymayday7@gmail.com>
2023-08-15 20:06:31 -0700

    Selecting the '.form'

--- js/app.js
+++ js/app.js
@@ -2,3 +2,6 @@ import getElement from "./utils/getElement.js";
 
 const url =
     'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=';
+
+const form = getElement('.form');
+console.log(form);
