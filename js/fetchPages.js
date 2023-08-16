fbba9f7fbb00afec2097403dc32b68324101eefb
stormymayday <stormymayday7@gmail.com>
2023-08-15 20:36:46 -0700

    Fetching the data using the url + searchValue

--- js/fetchPages.js
+++ js/fetchPages.js
@@ -1,5 +1,8 @@
 import getElement from "./utils/getElement.js";
 
+const url =
+    'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=';
+
 const resultsDiv = getElement('.results');
 
 const fetchPages = async (searchValue) => {
@@ -8,6 +11,12 @@ const fetchPages = async (searchValue) => {
     resultsDiv.innerHTML = `<div class="loading"></div>`;
 
     try {
+
+        const response = await fetch(`${url}${searchValue}`);
+
+        const data = await response.json();
+
+        console.log(data);
 
     } catch (error) {
 
