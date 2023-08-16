e3a6220f4ff8d58e442b64cde6c240b7a35d42da
stormymayday <stormymayday7@gmail.com>
2023-08-15 20:10:47 -0700

    Selecting the '.form-input'

--- js/app.js
+++ js/app.js
@@ -4,4 +4,5 @@ const url =
     'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=';
 
 const form = getElement('.form');
-console.log(form);
+const formInput = getElement('.form-input');
+console.log(formInput);
