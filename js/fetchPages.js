9b9b1244841b548d1feb78a5d79b9dccff34a07b
stormymayday <stormymayday7@gmail.com>
2023-08-15 20:32:09 -0700

    Displaying the '.loading' div

--- js/fetchPages.js
+++ js/fetchPages.js
@@ -4,6 +4,9 @@ const resultsDiv = getElement('.results');
 
 const fetchPages = async (searchValue) => {
 
+    // Displaying the loading div
+    resultsDiv.innerHTML = `<div class="loading"></div>`;
+
     try {
 
     } catch (error) {
